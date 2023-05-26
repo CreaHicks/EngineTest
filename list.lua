82861f6eaf13ea2ba786be950a210b79ab093a6a
CreaHicks <77201532+CreaHicks@users.noreply.github.com>
2023-05-26 21:10:40 +0200

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,6 +1,6 @@
 {
     {
-        name = "Animated Characters",
+        name = "Animated Characters 3",
         tags = "model, animated",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.png",
@@ -24,25 +24,25 @@
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/racing_kit.png",
     },
     {
-        name = "Stylized Nature",
+        name = "Ultimate Stylized Nature",
         tags = "model",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.png",
     },
     {
-        name = "Modular Ruins",
+        name = "Ultimate Modular Ruins",
         tags = "model",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.png",
     },
     {
-        name = "Modular Sci-Fi",
+        name = "Ultimate Modular Sci-Fi",
         tags = "model",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.png",
     },
     {
-        name = "Platformer",
+        name = "Platformer Pack",
         tags = "model, lowpoly",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/platformer_pack.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/platformer_pack.png",
