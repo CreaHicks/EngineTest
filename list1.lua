38d03d2e5d45e288cf2e31a9bacaa0658defc286
CreaHicks <77201532+CreaHicks@users.noreply.github.com>
2023-05-31 13:50:18 +0200

    Update list1.lua

--- list1.lua
+++ list1.lua
@@ -1,1 +1,71 @@
-
+{
+    {
+        name = "Stylized Animated Characters",
+        tags = "model, animated, stylized",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.png",
+    },
+    {
+        name = "Stylized Survival Kit",
+        tags = "model, lowpoly, stylized",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/survival_kit.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/survival_kit.png",
+    },
+    {
+        name = "Stylized Tower Defense Kit",
+        tags = "model, lowpoly, stylized",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/tower_defense_kit.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/tower_defense_kit.png",
+    },
+    {
+        name = "Stylized Racing Kit",
+        tags = "model, lowpoly, stylized",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/racing_kit.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/racing_kit.png",
+    },
+    {
+        name = "Stylized Nature",
+        tags = "model",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.png",
+    },
+    {
+        name = "Stylized Modular Ruins",
+        tags = "model, stylized",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.png",
+    },
+    {
+        name = "Stylized Modular Sci-Fi",
+        tags = "model, stylized",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.png",
+    },
+    {
+        name = "Stylized Platformer Pack",
+        tags = "model, lowpoly, stylized",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/platformer_pack.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/platformer_pack.png",
+    },
+    {
+        name = "Stylized Vehicle Sample",
+        tags = "scene, model, script, stylized",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/vehicle_sample.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/vehicle_sample.png",
+        root_install = true
+    },
+    {
+        name = "Stylized Portal effect",
+        tags = "scene, model, shader, material, stylized",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/portal.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/portal.png",
+        root_install = true
+    },
+    {
+        name = "Stylized Cars",
+        tags = "model, cars, material, stylized",
+        path = "https://raw.githubusercontent.com/CreaHicks/EngineTest/main/cars.zip",
+        thumbnail = "https://raw.githubusercontent.com/CreaHicks/EngineTest/main/cars_kit.lua",
+        root_install = true
+    }
+}
