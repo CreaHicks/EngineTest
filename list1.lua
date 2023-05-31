fa3e3c096de021326cddfc6841045141b17a900d
CreaHicks <77201532+CreaHicks@users.noreply.github.com>
2023-05-31 13:52:23 +0200

    Update list1.lua

--- list1.lua
+++ list1.lua
@@ -65,7 +65,7 @@
         name = "Stylized Cars",
         tags = "model, cars, material, stylized",
         path = "https://raw.githubusercontent.com/CreaHicks/EngineTest/main/cars.zip",
-        thumbnail = "https://raw.githubusercontent.com/CreaHicks/EngineTest/main/cars_kit.lua",
+        thumbnail = "https://raw.githubusercontent.com/CreaHicks/EngineTest/main/cars_kit.png",
         root_install = true
     }
 }
