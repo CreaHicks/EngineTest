366662df008e783678d3d64cd1b7e899f6aed73a
CreaHicks <77201532+CreaHicks@users.noreply.github.com>
2023-05-31 13:42:08 +0200

    Update list.lua

--- list.lua
+++ list.lua
@@ -24,19 +24,19 @@
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/racing_kit.png",
     },
     {
-        name = "Ultimate Stylized Nature",
+        name = "Stylized Nature",
         tags = "model",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.png",
     },
     {
-        name = "Ultimate Stylized Modular Ruins",
+        name = "Stylized Modular Ruins",
         tags = "model, stylized",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.png",
     },
     {
-        name = "Ultimate Stylized Modular Sci-Fi",
+        name = "Stylized Modular Sci-Fi",
         tags = "model, stylized",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.png",
